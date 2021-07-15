4d42c45f9d3639f7a520ba8b72f0eacb758c3677
FallenBlood <67326508+fallenblood7080@users.noreply.github.com>
2021-07-15 10:14:56 +0530

    Add files via upload

---
+++ Platformer_AI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformer_AI : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    public Vector2 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        rb.position += dir * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Point"))
+        {
+            dir.x *= -1;
+            transform.localScale = new Vector3(-dir.x, 1, 1);
+        }
+    }
+}
