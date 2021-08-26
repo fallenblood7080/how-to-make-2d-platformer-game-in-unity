2a5cd966324ea8398ca03a0a2e37c1c59256228e
FallenBlood <67326508+fallenblood7080@users.noreply.github.com>
2021-08-26 11:43:56 +0530

    Add files via upload

---
+++ enemyDead.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDead : MonoBehaviour
+{
+    public GameObject deadEffect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //kill
+            Instantiate(deadEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+    }
+}
