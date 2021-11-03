315761f0b3233a2fd41999784b996eeb184723e1
FallenBlood <67326508+fallenblood7080@users.noreply.github.com>
2021-11-03 18:35:32 +0530

    Add files via upload

---
+++ SceneManagentmentScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagentmentScript : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void PlayGame(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void mainmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void loadnextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
